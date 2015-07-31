6cc9a11a9e251de7cdcfe51e9a02e0ff423f2eb7
David Weinstein <dweinst@insitusec.com>
2015-07-31 14:04:25 -0400

    add details

--- bin/details.js
+++ bin/details.js
@@ -2,10 +2,10 @@
 'use strict';
 
 const co = require('co');
-const rc = require('./config');
+const rc = require('../config');
 const assert = require('assert');
-const debug = require('./debug')('bin');
-const getApkInfo = require('./get-apk-info');
+const debug = require('../debug')('bin');
+const getApkInfo = require('../get-apk-info');
 const request = require('request');
 const fs = require('fs');
 
@@ -15,15 +15,7 @@ co(function*() {
   const url = rc.url;
   assert(id, 'require app id');
   const info = yield getApkInfo(url, id);
-  debug('requesting apk from', info.apk.path);
-  const out = rc.o ? fs.createWriteStream(rc.o) : process.stdout;
-  const stream = request(info.apk.path, {
-    gzip: true, headers: {'User-Agent': ''}
-  });
-  stream.pipe(out);
-  stream.on('end', function () {
-    debug('stream ended');
-  });
+  console.log('%j', info);
 })
   .catch(function (err) {
     console.error(err.stack);
